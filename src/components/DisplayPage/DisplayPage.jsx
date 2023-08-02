dfb30566a5887980b613e1b988a7526bab84e139
Steve Shumaker <steve.j.shumaker@gmail.com>
2023-08-01 22:28:23 -0500

    mui cards for display, still figuring out styling

--- src/components/DisplayPage/DisplayPage.jsx
+++ src/components/DisplayPage/DisplayPage.jsx
@@ -1,6 +1,10 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import ExperienceToDisplay from "../ExperienceToDisplay/ExperienceToDisplay.jsx";
+// MUI
+import Box from "@mui/material/Box";
+import { Container, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid";
 
 function DisplayPage() {
   // init dispatch
@@ -17,14 +21,36 @@ function DisplayPage() {
 
   return (
     <div>
-      {experiences.map((experience) => {
-        return (
-          <div key={experience.this_id}>
-            <ExperienceToDisplay experience={experience} />
-            <hr />
-          </div>
-        );
-      })}
+      <Box
+        sx={{
+          bgcolor: "background.paper",
+          pt: 8,
+          pb: 6,
+        }}
+      >
+        <Container maxWidth="sm">
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="text.primary"
+            gutterBottom
+          >
+            Username Placeholder
+          </Typography>
+        </Container>
+      </Box>
+
+
+          {experiences.map((experience) => {
+            return (
+              <ExperienceToDisplay
+                key={experience.this_id}
+                experience={experience}
+              />
+            );
+          })}
+
     </div>
   );
 }
--- src/components/ExperienceToDisplay/ExperienceToDisplay.jsx
+++ src/components/ExperienceToDisplay/ExperienceToDisplay.jsx
@@ -1,5 +1,18 @@
 import { useDispatch } from "react-redux";
 import { useState } from "react";
+//MUI
+import {
+  Card,
+  CardMedia,
+  CardActions,
+  CardContent,
+  Button,
+  Typography,
+  Link,
+  Input,
+  Container,
+  Grid,
+} from "@mui/material";
 
 function ExperienceToDisplay({ experience }) {
   // local states for editing
@@ -44,74 +57,134 @@ function ExperienceToDisplay({ experience }) {
   };
 
   return (
-    <>
-      {isEditing ? (
-        <>
-          <p>{experience.name}</p>
-          <input
-            value={editToSend.description}
-            onChange={(e) => {
-              setEditToSend({
-                ...editToSend,
-                description: e.target.value,
-              });
-            }}
-          />
-          <p>{experience.location_desc}</p>
-          <input
-            value={editToSend.web_path}
-            onChange={(e) => {
-              setEditToSend({
-                ...editToSend,
-                web_path: e.target.value,
-              });
-            }}
-          />
-          <button onClick={() => sendUpdate(experience.this_id)}>Save</button>
-          <button onClick={() => setEditingId(null)}>Cancel</button>
-        </>
-      ) : (
-        <>
-          <div>
-            <p>
-              <img src={experience.photo_path} />
-            </p>
-          </div>
-          <div>
-            <p>{experience.name}</p>
-            <p>{experience.description}</p>
-            <p>{experience.location_desc}</p>
-
-            {experience.rating ? <p>Reviews: {experience.rating}</p> : null}
-            {experience.stars ? (
-              <img src={`/yelp_images/small_${Number(experience.stars)}.png`} />
-            ) : null}
-            <p>
-              {experience.web_path ? (
-                <a target="_blank" href={experience.web_path}>
-                  Link
-                </a>
-              ) : (
-                <a
-                  target="_blank"
-                  href={`https://www.google.com/search?q=${experience.name}${experience.location_desc}`}
+    <div key={experience.this_id}>
+      <Container sx={{ py: 8 }} maxWidth="md">
+        <Grid container spacing={4}>
+          {isEditing ? (
+            <>
+              <Card
+                sx={{
+                  height: "100%",
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                <CardMedia
+                  component="div"
+                  sx={{
+                    // 16:9
+                    pt: "56.25%",
+                  }}
+                  image={experience.photo_path}
+                />
+                <CardContent sx={{ flexGrow: 1 }}>
+                  <Typography>{experience.name}</Typography>
+                  <Input
+                    value={editToSend.description}
+                    onChange={(e) => {
+                      setEditToSend({
+                        ...editToSend,
+                        description: e.target.value,
+                      });
+                    }}
+                  />
+                  <p>{experience.location_desc}</p>
+                  <Input
+                    value={editToSend.web_path}
+                    onChange={(e) => {
+                      setEditToSend({
+                        ...editToSend,
+                        web_path: e.target.value,
+                      });
+                    }}
+                  />
+                  <Button onClick={() => sendUpdate(experience.this_id)}>
+                    Save
+                  </Button>
+                  <Button onClick={() => setEditingId(null)}>Cancel</Button>
+                </CardContent>
+              </Card>
+            </>
+          ) : (
+            <>
+              <Grid item key={experience.this_id} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
                 >
-                  Search
-                </a>
-              )}
-            </p>
-            {experience.favorite ? <p>Fav</p> : null}
-            <button onClick={() => handleFavoriteClick(experience)}>
-              Heart
-            </button>
-            <button onClick={() => handleEditClick(experience)}>Edit</button>
-            <button onClick={() => handleDeleteClick(experience.this_id)}>
-              Delete
-            </button>
-          </div>
-        </>
-      )}
-    </>
+                  <CardMedia
+                    component="div"
+                    sx={{
+                      // 16:9
+                      pt: "56.25%",
+                    }}
+                    image={experience.photo_path}
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {experience.name}
+                      {experience.favorite ? <span> ❤️</span> : null}
+                    </Typography>
+                    <Typography>{experience.description}</Typography>
+                    <Typography>{experience.location_desc}</Typography>
+                    {experience.rating ? (
+                      <Typography>Reviews: {experience.rating}</Typography>
+                    ) : null}
+
+                    {experience.stars ? (
+                      <img
+                        src={`/yelp_images/small_${Number(
+                          experience.stars
+                        )}.png`}
+                      />
+                    ) : null}
+                    {experience.web_path ? (
+                      <Typography>
+                        <Link target="_blank" href={experience.web_path}>
+                          Link Placeholder
+                        </Link>
+                      </Typography>
+                    ) : (
+                      <Typography>
+                        <Link
+                          target="_blank"
+                          href={`https://www.google.com/search?q=${experience.name}${experience.location_desc}`}
+                        >
+                          Search
+                        </Link>
+                      </Typography>
+                    )}
+                  </CardContent>
+                  <CardActions>
+                    <Button
+                      onClick={() => handleFavoriteClick(experience)}
+                      size="small"
+                    >
+                      ❤️
+                    </Button>
+                    <Button
+                      onClick={() => handleEditClick(experience)}
+                      size="small"
+                    >
+                      Edit
+                    </Button>
+                    <Button
+                      onClick={() => handleDeleteClick(experience.this_id)}
+                      size="small"
+                    >
+                      Edit
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            </>
+          )}
+        </Grid>
+      </Container>
+    </div>
   );
 }
 
