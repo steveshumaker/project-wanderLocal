e1c7cad928fcf32a063b1dbdcaa49a6f41fe6433
Steve Shumaker <steve.j.shumaker@gmail.com>
2023-08-01 23:08:52 -0500

    minor formatting stuff

--- src/components/DisplayPage/DisplayPage.jsx
+++ src/components/DisplayPage/DisplayPage.jsx
@@ -2,22 +2,11 @@ import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import ExperienceToDisplay from "../ExperienceToDisplay/ExperienceToDisplay.jsx";
 // MUI
-import AppBar from "@mui/material/AppBar";
-import Button from "@mui/material/Button";
-import CameraIcon from "@mui/icons-material/PhotoCamera";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import CssBaseline from "@mui/material/CssBaseline";
 import Grid from "@mui/material/Grid";
-import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import Link from "@mui/material/Link";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 function DisplayPage() {
   // init dispatch
@@ -31,6 +20,8 @@ function DisplayPage() {
 
   // pulls the experiences array from the store
   const experiences = useSelector((store) => store.experience);
+
+  const user = useSelector((store) => store.user);
 
   return (
     <div>
@@ -51,11 +42,14 @@ function DisplayPage() {
               color="text.primary"
               gutterBottom
             >
-              Username Placeholder
+              {user.username}'s experiences.
+            </Typography>
+            <Typography variant="h5" align="center" color="text.secondary" paragraph>
+              click ❤️ to favorite
             </Typography>
           </Container>
         </Box>
-        <Container sx={{ py: 8 }} maxWidth="md">
+        <Container sx={{ py: 1 }} maxWidth="md">
           <Grid container spacing={4}>
             {experiences.map((experience) => {
               return (
