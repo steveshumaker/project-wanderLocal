d5970bf159f504906dd903ac93fc6287aa254419
Steve Shumaker <steve.j.shumaker@gmail.com>
2023-08-15 21:07:51 -0500

    working table, review search is wonky and need to implement filter

--- src/components/SearchPage/SearchPage.jsx
+++ src/components/SearchPage/SearchPage.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect, Fragment } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import ExperienceToDisplay from "../ExperienceToDisplay/ExperienceToDisplay";
+import AlternateSearchPage from "../AlternateSearchPage/AlternateSearchPage";
 // MUI
 import Box from "@mui/material/Box";
 import { CssBaseline } from "@mui/material";
@@ -27,6 +28,9 @@ function SearchPage() {
   // Render state
   const [shouldRender, setShouldRender] = useState(false);
 
+  // Alternate search state
+  const [altState, setAltState] = useState(false);
+
   // Tags array manipulation - flatten and remove dupes
   const flatTags = tagList.flat();
   const finalTags = [...new Set(flatTags)];
@@ -44,9 +48,7 @@ function SearchPage() {
     console.log("PAGE LOADED");
     // Set the tag list on page load
     dispatch({ type: "FETCH_ALL_EXPERIENCES" });
-    setTimeout(() => {
-      
-    }, 1000);
+    setTimeout(() => {}, 1000);
     if (experiences.length > 0) {
       experiences.map((experience) => {
         if (experience.tags !== null) {
@@ -63,90 +65,96 @@ function SearchPage() {
 
   return (
     <div>
-      {/* ITEM DISPLAY */}
-
-      <div>
-        <CssBaseline />
-        <main>
-          <Box
-            sx={{
-              bgcolor: "background.paper",
-              pt: 8,
-              pb: 2,
-            }}
-          >
-            <Container maxWidth="sm">
-              <Typography
-                component="h1"
-                variant="h2"
-                align="center"
-                color="text.primary"
-                gutterBottom
-              >
-                experiences.
-              </Typography>
-              <Autocomplete
-                sx={{ width: "500px" }}
-                multiple
-                id="tags-outlined"
-                options={finalTags}
-                getOptionLabel={(tag) => tag}
-                filterSelectedOptions
-                value={search}
-                onChange={handleTagsChange}
-                renderInput={(params) => (
-                  <TextField {...params} label="search." placeholder="tags." />
-                )}
-              />
-            </Container>
-          </Box>
-          {search.length === 0 ? (
-            <Container sx={{ py: 2 }} maxWidth="lg">
-              <Grid container spacing={2}>
-                {experiences.map((experience) => {
-                  return (
-                    <Grid item key={experience.this_id} xs={18} sm={9} md={4}>
-                      <ExperienceToDisplay
-                        key={experience.this_id}
-                        experience={experience}
-                      />
-                    </Grid>
-                  );
-                })}
-              </Grid>
-            </Container>
-          ) : (
-            <Container sx={{ py: 1 }} maxWidth="md">
-              <Grid container spacing={4}>
-                {experiences.map((experience) => {
-                  // can probably set tags to default as empty
-                  // and change this to a filter
-                  for (let searchTerm of search) {
-                    if (experience.tags) {
-                      if (experience.tags.includes(searchTerm)) {
-                        return (
-                          <Grid
-                            item
-                            key={experience.this_id}
-                            xs={18}
-                            sm={9}
-                            md={6}
-                          >
-                            <ExperienceToDisplay
+      {!altState ? (
+        <AlternateSearchPage />
+      ) : (
+        <div>
+          <CssBaseline />
+          <main>
+            <Box
+              sx={{
+                bgcolor: "background.paper",
+                pt: 8,
+                pb: 2,
+              }}
+            >
+              <Container maxWidth="sm">
+                <Typography
+                  component="h1"
+                  variant="h2"
+                  align="center"
+                  color="text.primary"
+                  gutterBottom
+                >
+                  experiences.
+                </Typography>
+                <Autocomplete
+                  sx={{ width: "500px" }}
+                  multiple
+                  id="tags-outlined"
+                  options={finalTags}
+                  getOptionLabel={(tag) => tag}
+                  filterSelectedOptions
+                  value={search}
+                  onChange={handleTagsChange}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      label="search."
+                      placeholder="tags."
+                    />
+                  )}
+                />
+              </Container>
+            </Box>
+            {search.length === 0 ? (
+              <Container sx={{ py: 2 }} maxWidth="lg">
+                <Grid container spacing={2}>
+                  {experiences.map((experience) => {
+                    return (
+                      <Grid item key={experience.this_id} xs={18} sm={9} md={4}>
+                        <ExperienceToDisplay
+                          key={experience.this_id}
+                          experience={experience}
+                        />
+                      </Grid>
+                    );
+                  })}
+                </Grid>
+              </Container>
+            ) : (
+              <Container sx={{ py: 1 }} maxWidth="md">
+                <Grid container spacing={4}>
+                  {experiences.map((experience) => {
+                    // can probably set tags to default as empty
+                    // and change this to a filter
+                    for (let searchTerm of search) {
+                      if (experience.tags) {
+                        if (experience.tags.includes(searchTerm)) {
+                          return (
+                            <Grid
+                              item
                               key={experience.this_id}
-                              experience={experience}
-                            />
-                          </Grid>
-                        );
+                              xs={18}
+                              sm={9}
+                              md={6}
+                            >
+                              <ExperienceToDisplay
+                                key={experience.this_id}
+                                experience={experience}
+                              />
+                            </Grid>
+                          );
+                        }
                       }
                     }
-                  }
-                })}
-              </Grid>
-            </Container>
-          )}
-        </main>
-      </div>
+                  })}
+                </Grid>
+              </Container>
+            )}
+          </main>
+        </div>
+      )}
       {/* END ITEM DISPLAY */}
     </div>
   );
